2a558a46f3713a8bb2ab12c26d411e2c3a7dd8d4
sharry <sharry2578@126.com>
2019-05-14 16:53:31 +0800

    5/14 v4

--- react-webpack-2/src/components/Component2.jsx
+++ react-webpack-2/src/components/Component2.jsx
@@ -2,7 +2,7 @@ import React from '../../node_modules/react'
 import cssObj from '@/css/index.css'
 console.log(cssObj);
 
-export default function Comonent2(props){
+export default function Comonent2(props){ 
     console.log(props);
     return <div className={cssObj.class2} >component2 <hr/> {props.name} -- {props.age} -- {props.gender} <hr/> 组件</div>
 }
